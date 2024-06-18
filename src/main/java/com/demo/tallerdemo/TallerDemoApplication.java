2d0eb7b13c458b8fff679eb2484e7d5dcefc3b89
TonyMaceda <antony.maceda.choque@gmail.com>
2024-06-18 11:04:51 -0400

    Creación de proyecto demo taller

---
+++ src/main/java/com/demo/tallerdemo/TallerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tallerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/tallerdemo/TallerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.tallerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
